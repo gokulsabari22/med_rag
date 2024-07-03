b44f564999cd5e3df941ccf8099ca10c2b1a9f99
Gokul Gowri Shankar <gokul.gowrishankar@wavicledata.com>
2024-07-04 00:39:00 +0530

    Changed answer_grader type from 'bool' to 'str'

--- graph/chains/answer_grader.py
+++ graph/chains/answer_grader.py
@@ -2,6 +2,7 @@ from langchain_groq import ChatGroq
 from langchain_core.pydantic_v1 import BaseModel, Field
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnableSequence
+from langchain.schema import Document
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,14 +10,16 @@ load_dotenv()
 llm = ChatGroq(model="llama3-8b-8192", temperature=0)
 
 class AnswerGrader(BaseModel):
-    binary_score: bool = Field(
+    binary_score: str = Field(
         description = "Answer addresses the question"
     )
 
 structured_llm_grader = llm.with_structured_output(AnswerGrader)
 
-system = """You are a grader assessing whether an answer address / resolves a question \n
-             Give a binary score 'yes' or 'no'. 'yes' means the answer resolves a question"""
+system = """You are a grader assessing whether an answer addresses / resolves a question.
+            Give a binary score 'yes' or 'no'. 'yes' means the answer resolves the question, 'no' means it doesn't.
+            Respond with ONLY 'yes' or 'no'."""
+
 
 answer_prompt = ChatPromptTemplate.from_messages([
     ("system", system),
@@ -24,3 +27,8 @@ answer_prompt = ChatPromptTemplate.from_messages([
 ])
 
 answer_grader: RunnableSequence = answer_prompt | structured_llm_grader
+
+if __name__ == "__main__":
+    documents = """1 Introduction  \nCondition  \nRheumatoid arthritis (RA)  is an autoimmune systemic inflammatory arthritis.  RA affects 1 \npercent of the world’s population, including more than 1 million American adults.48 RA is \ncharacterized by  synovial inflammation  of joints, which can lead to progressive erosion of  bone , \nirreversible damage to the joint, loss of function, and resultant disability. The average incidence \nof RA in the United States is approximately 70 per 100,000 adults annually.49 RA can develop at \nany age , but incidence increases with age, peaking in the fifth decade .50 The i ncidence of RA is 2 \nto 3 times higher in women.  \nEtiology  \nThe etiology of RA is incompletely understood, but m ultiple environmental and genetic"""
+    res = answer_grader.invoke({"question": "What causes Arthesis", "generation": documents})
+    print(res.binary_score)
--- graph/graph.py
+++ graph/graph.py
@@ -1,6 +1,8 @@
 from langgraph.graph import END, StateGraph
 from graph.const import RETRIEVE, GRADE_DOCUMENT, GENERATE, WEBSEARCH
 from graph.nodes import generate, grade_document, retrieve, websearch
+from graph.chains.hallucination_grader import hallunication_grader
+from graph.chains.answer_grader import answer_grader 
 from graph.state import GraphState
 from dotenv import load_dotenv
 
@@ -18,6 +20,28 @@ def decide_to_generate(state):
     else:
         print("---DECISION: GENERATE---")
         return GENERATE
+    
+def grade_generation_grounded_in_documents_and_question(state: GraphState) -> str:
+    question = state["question"]
+    documents = state["documents"]
+    generation = state["generation"]
+
+    score = hallunication_grader.invoke({"documents": documents, "generation": generation})
+    print(f"SCORE: {score.binary_score}")
+    if hallunication_grade := score.binary_score:
+        print("-----------GENERATION IS GROUNDED IN DOCUMENT--------------------")
+        score = answer_grader.invoke({"question": question, "generation": generation})
+        
+        if answer_grade := score.binary_score:
+            print("-----------------GENERATED ANSWER ADDRESS QUESTION---------------")
+            return "useful"
+        else:
+            print("--------------------GENERATED ANSWER DOES NOT ADDRESSES QUESTION----------------")
+            return "not useful"
+    else:
+        print("------------------GENERATED ANSWER IS NOT PRESENT IN THE DOCUMENT-----------------")
+        return "not supported"
+
 
 
 workflow = StateGraph(GraphState)
@@ -29,6 +53,7 @@ workflow.add_node(WEBSEARCH, websearch)
 
 workflow.set_entry_point(RETRIEVE)
 workflow.add_edge(RETRIEVE, GRADE_DOCUMENT)
+
 workflow.add_conditional_edges(
     GRADE_DOCUMENT,
     decide_to_generate,
@@ -37,9 +62,20 @@ workflow.add_conditional_edges(
         GENERATE: GENERATE,
     },
 )
+
+workflow.add_conditional_edges(
+    GENERATE,
+    grade_generation_grounded_in_documents_and_question,
+    {
+        "not supported": GENERATE,
+        "useful": END,
+        "not useful": WEBSEARCH
+    }
+)
+
 workflow.add_edge(WEBSEARCH, GENERATE)
 workflow.add_edge(GENERATE, END)
 
 app = workflow.compile()
 
-# app.get_graph().draw_mermaid_png(output_file_path="graph.png")
+app.get_graph().draw_mermaid_png(output_file_path="graph.png")
